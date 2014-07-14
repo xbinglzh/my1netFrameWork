4f755556e987e19bba73d8a49e0bb241b34740c9
xUanBing <xbinglzh@gmail.com>
2014-07-15 00:32:10 +0800

    删除 HappyMouse 工程
    删除 HappyMouse 工程

--- cocos2d-x-2.2.2/projects/HappyMouse/Classes/Model/object/GameObject.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  GameObject.cpp
-//  motoGame
-//
-//  Created by xUanBing on 14-4-15.
-//
-//
-
-#include "GameObject.h"
--- cocos2d-x-2.2.2/projects/HappyMouse/Classes/View/BattleView/GameBattleView.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  GameBattleView.cpp
-//  HappyMouse
-//
-//  Created by xUanBing on 14-7-13.
-//
-//
-
-#include "GameBattleView.h"
