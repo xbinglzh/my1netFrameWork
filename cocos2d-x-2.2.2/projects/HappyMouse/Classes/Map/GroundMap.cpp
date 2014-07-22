5603c37e34cd7b75d78551f86d753c462c7395b4
xUanBing <xbinglzh@gmail.com>
2014-07-23 00:37:05 +0800

    GroundMap
    GroundMap

---
+++ cocos2d-x-2.2.2/projects/HappyMouse/Classes/Map/GroundMap.cpp
@@ -0,0 +1,135 @@
+//
+//  GroundMap.cpp
+//  HappyMouse
+//
+//  Created by xUanBing on 14-7-22.
+//
+//
+
+#include "GroundMap.h"
+#include "LayoutUtil.h"
+
+#define Base_Layer_Tag            1001
+#define Adorn_Bottom_Layer_Tag    1002
+#define Game_Layer_Tag            1003
+#define Drops_Layer_Tag           1004
+#define Npc_Layer_Tag             1005
+#define Adorn_Top_Layer_Tag       1006
+#define Guide_Layer_Tag           1007
+#define Gray_Layer_Tag            1008
+
+enum LayerZOrder {
+    
+    BaseLayerZorder               =  0,
+    AdornBottomLayerZorder,
+    GameLayerZorder,
+    DropsLayerZorder,
+    NpcLayerZorder,
+    AdornTopLayerZorder,
+    GuideLayerZorder,
+    GrayLayerZorder
+    
+};
+
+GroundMap::GroundMap() :
+_baseLayer(NULL),
+_adornBottomLayer(NULL),
+_gameLayer(NULL),
+_dropsLayer(NULL),
+_npcLayer(NULL),
+_adornTopLayer(NULL),
+_guideLayer(NULL),
+_grayLayer(NULL),
+_mapId(-1) {
+    
+}
+
+GroundMap::~GroundMap() {
+    
+}
+
+GroundMap* GroundMap::create(const char *fileName) {
+    
+    GroundMap* self = new GroundMap();
+    
+    if (self && self->initWithFile(fileName)) {
+        self->autorelease();
+        return self;
+    }
+    
+    CC_SAFE_DELETE(self);
+    return NULL;
+}
+
+bool GroundMap::initWithFile(const char *fileName) {
+    this->setContentSize(CCSizeMake(CCDirector::sharedDirector()->getOpenGLView()->getDesignResolutionSize().width, CCDirector::sharedDirector()->getOpenGLView()->getDesignResolutionSize().height));
+    this->setAnchorPoint(CCPointZero);
+    
+    _baseLayer = genLayerColor(BaseLayerZorder, Base_Layer_Tag);
+    _adornBottomLayer = genLayerColor(AdornBottomLayerZorder, Adorn_Bottom_Layer_Tag);
+    _gameLayer = genLayerColor(GameLayerZorder, Game_Layer_Tag);
+    _dropsLayer = genLayerColor(DropsLayerZorder, Drops_Layer_Tag);
+    _npcLayer = genLayerColor(NpcLayerZorder, Npc_Layer_Tag);
+    _adornTopLayer = genLayerColor(AdornTopLayerZorder, Adorn_Top_Layer_Tag);
+    _guideLayer = genLayerColor(GuideLayerZorder, Guide_Layer_Tag);
+    _grayLayer = genLayerColor(GrayLayerZorder, Gray_Layer_Tag);
+    
+    return true;
+}
+
+void GroundMap::pauseBattle() {
+    changeLayerChildState(_gameLayer, true);
+    changeLayerChildState(_dropsLayer, true);
+    changeLayerChildState(_npcLayer, true);
+    changeLayerChildState(_guideLayer, true);
+}
+
+void GroundMap::resumeBattle() {
+    changeLayerChildState(_gameLayer, false);
+    changeLayerChildState(_dropsLayer, false);
+    changeLayerChildState(_npcLayer, false);
+    changeLayerChildState(_guideLayer, false);
+}
+
+CCLayerColor* GroundMap::genLayerColor(int zOrder, int layerTag) {
+    CCLayerColor* layer = CCLayerColor::create(ccc4(0, 0, 0, 0));
+    layer->setAnchorPoint(CCPointZero);
+    layer->setContentSize(this->getContentSize());
+    this->addChild(layer, zOrder, layerTag);
+    LayoutUtil::layoutParentBottomLeft(layer);
+    
+    return layer;
+}
+
+void GroundMap::topLayerTurnGray(bool isTurnGray) {
+    if (_grayLayer) {
+        _grayLayer->setOpacity(isTurnGray ? 120 : 0);
+    }
+}
+
+void GroundMap::changeLayerChildState(CCNode* parent,bool isPause) {
+    CCArray* array = NULL;
+    CCObject* object = NULL;
+    CCNode* child = NULL;
+    
+    if(parent) {
+        array = parent->getChildren();
+        CCARRAY_FOREACH(array, object) {
+            child = (CCNode*)object;
+            if(isPause)
+                child->pauseSchedulerAndActions();
+            else
+                child->resumeSchedulerAndActions();
+        }
+    }
+}
+
+void GroundMap::setMapId(int MapId) {
+    _mapId = MapId;
+}
+
+int GroundMap::getMapId() const {
+    return _mapId;
+}
+
+
---
+++ cocos2d-x-2.2.2/projects/HappyMouse/Classes/Map/GroundMap.h
@@ -0,0 +1,57 @@
+//
+//  GroundMap.h
+//  HappyMouse
+//
+//  Created by xUanBing on 14-7-22.
+//
+//
+
+#ifndef __HappyMouse__GroundMap__
+#define __HappyMouse__GroundMap__
+
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class GroundMap : public cocos2d::CCNode {
+    
+public:
+    GroundMap();
+    virtual ~GroundMap();
+    
+public:
+    static GroundMap* create(const char* fileName);
+    
+public:
+    bool initWithFile(const char* fileName);
+    
+public:
+    void pauseBattle();
+    void resumeBattle();
+    void changeLayerChildState(CCNode* parent,bool isPause);
+    
+public:
+    void setMapId(int MapId);
+    int  getMapId() const;
+    
+private:
+    CCLayerColor* genLayerColor(int zOrder, int layerTag);
+    void topLayerTurnGray(bool isTurnGray);
+    
+private:
+    CCLayerColor*  _baseLayer;         //  基础Layer, 存放最底层背景
+    CCLayerColor*  _adornBottomLayer;  //  修饰层，背景修饰层面
+    
+    CCLayerColor*  _gameLayer;         //  游戏层
+    CCLayerColor*  _dropsLayer;        //  掉落层
+    CCLayerColor*  _npcLayer;          //  npc层
+    
+    CCLayerColor*  _adornTopLayer;     //  上层修饰
+    CCLayerColor*  _guideLayer;        //  新手引导层
+    CCLayerColor*  _grayLayer;         //  灰色遮罩
+    
+    int _mapId;
+};
+
+
+#endif /* defined(__HappyMouse__GroundMap__) */
